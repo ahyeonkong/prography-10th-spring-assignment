57c2aa21462e758941815df4a6e0a49f40eeea3e
ahyeonkong <kimah0403@gmail.com>
2025-02-16 18:10:57 +0900

    feat: User 엔티티 생성

---
+++ src/main/java/assignment/game/PingPong/domain/user/entity/Status.java
@@ -0,0 +1,7 @@
+package assignment.game.PingPong.domain.user.entity;
+
+public enum Status {
+    WAIT, // 대기
+    ACTIVE, // 활성
+    NON_ACTIVE // 비활성
+}
---
+++ src/main/java/assignment/game/PingPong/domain/user/entity/User.java
@@ -0,0 +1,33 @@
+package assignment.game.PingPong.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "\"User\"")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer fakerId;
+
+    private String name;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
