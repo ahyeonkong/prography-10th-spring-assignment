efd0d38b3cc5bb8efcb8e9c2f8b34af7e2895825
ahyeonkong <kimah0403@gmail.com>
2025-02-16 18:13:28 +0900

    feat: UserRoom 엔티티 생성

---
+++ src/main/java/assignment/game/PingPong/domain/userRoom/entity/Team.java
@@ -0,0 +1,6 @@
+package assignment.game.PingPong.domain.userRoom.entity;
+
+public enum Team {
+    RED,
+    BLUE
+}
---
+++ src/main/java/assignment/game/PingPong/domain/userRoom/entity/UserRoom.java
@@ -0,0 +1,29 @@
+package assignment.game.PingPong.domain.userRoom.entity;
+
+import assignment.game.PingPong.domain.room.entity.Room;
+import assignment.game.PingPong.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "UserRoom")
+@Getter
+@Setter
+public class UserRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id") // "room_id" 컬럼이 Room 엔티티의 ID를 참조
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") // "user_id" 컬럼이 User 엔티티의 ID를 참조
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private Team team;
+
+}
